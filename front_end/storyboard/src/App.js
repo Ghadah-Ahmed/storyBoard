c0bc55c1ecf98d5164b1a65efa50ec1968fa9e91
Masha Aldosari <md.webs95@gmail.com>
2021-11-16 10:22:02 +0300

    style

--- front_end/storyboard/src/App.js
+++ front_end/storyboard/src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 import Display from "./components/displayComonents/Display";
 import Home from './components/homeComponents/home'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
-import Library from './components/Library';
+import Library from './components/library';
 import Login from './components/login';
 
 export default function App() {
